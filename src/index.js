2765b689eac644a9063ee04b7173ca8b19691569
chocolate-peanut <limschyi@gmail.com>
2023-08-09 15:37:42 +0800

    [FIX]Routing issue solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import PeopleTable from 'PeopleTable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/people" element={<PeopleTable />} />
+    </Routes>
+  </BrowserRouter>
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
